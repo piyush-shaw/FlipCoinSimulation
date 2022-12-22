e252a718cd33be14834fe54f2c553c17f10438c8
piyush-shaw <piyushshaw0000@gmail.com>
2022-12-22 19:58:22 +0530

    to check number of times head or tail won

--- flipCoinSimulator.sh
+++ flipCoinSimulator.sh
@@ -8,4 +8,21 @@ else
 echo "Tails"
 fi
 
+flips=1
+heads=0
+tails=0
+while [ $flips -le 20 ]
+do
+((flips++))
+if [ $((RANDOM%2)) -eq 1 ]
+then
+echo "head wins"
+((heads++))
+else
+echo "tail wins"
+((tails++))
+fi
+echo "total heads are :" $heads "total tails are :" $tails
+done
+
 
